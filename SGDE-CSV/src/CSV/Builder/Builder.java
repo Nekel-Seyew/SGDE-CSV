ac0705645a0d4d77eacd396ff8ad84bb44ab66bb
kdsweenx <kdsweenx@KDSWEENX-MOBL1.amr.corp.intel.com>
2013-08-28 10:52:17 -0700

    First Commit
    well, second I guess

---
+++ SGDE-CSV/src/CSV/Builder/Builder.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CSV.Builder;
+
+/**
+ *
+ * @author kdsweenx
+ */
+public class Builder {
+    
+}
---
+++ SGDE-CSV/src/CSV/Parser/Parser.java
@@ -0,0 +1,14 @@
+package CSV.Parser;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kdsweenx
+ */
+public class Parser {
+    
+}
