11ac63d89c26c2331eb8a7e58f2beefd3d75b1cc
EddieW <eddiex.wang@intel.com>
2013-08-28 12:00:52 -0700

    blah

--- SGDE-CSV/src/CSV/Builder/Builder.java
+++ SGDE-CSV/src/CSV/Builder/Builder.java
@@ -4,12 +4,34 @@
  */
 package CSV.Builder;
 
+import java.io.IOException;
+import java.lang.StringBuilder;
+import java.util.Hashtable;
+
 /**
  *
  * @author kdsweenx
  */
-public static class Builder {
-    public static void makeCSVFile(){
+public class Builder {
+    private static StringBuilder fileContents;
+    public static void makeCSVFile(Database db){
+        for(Hashtable row : db){
+            for(Object obj : row){
+                fileContents.append(obj.toString());
+                fileContents.append(',');
+            }
+            fileContents.append('\n');
+        }
 
+    }
+    public static void makeCSVFile(Database db, Path filePath) throws IOException {
+        for(Hashtable row : db){
+            for(Object obj : row){
+                fileContents.append(obj.toString());
+                fileContents.append(',');
+            }
+            fileContents.append('\n');
+        }
+        Files.newBufferedWriter(filePath, fileContents).write(s, 0, s.length());
     }
 }
